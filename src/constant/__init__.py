3bc0997aa904848c56b767bed3f0303bb8ba439d
Vignesh <voddamvignesh0707@gmail.com>
2024-09-16 22:22:51 +0530

    Added modules

---
+++ src/constant/__init__.py
@@ -0,0 +1,16 @@
+import os
+
+
+AWS_S3_BUCKET_NAME = "wafer-fault"
+MONGO_DATABASE_NAME = "pwskills"
+MONGO_COLLECTION_NAME = "waferfault"
+
+
+TARGET_COLUMN = "quality"
+MONGO_DB_URL="mongodb+srv://imran:TdPLW9Ad0OzpSSD2@cluster0.fv0lm61.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+
+MODEL_FILE_NAME = "model"
+MODEL_FILE_EXTENSION = ".pkl"
+
+artifact_folder =  "artifacts"
