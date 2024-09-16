6e8cfb9a334c2f4600023e3c60bc697de194d676
Vignesh <voddamvignesh0707@gmail.com>
2024-09-16 23:31:22 +0530

    Added Pymongo

--- src/constant/__init__.py
+++ src/constant/__init__.py
@@ -1,13 +1,13 @@
 import os
 
 
-AWS_S3_BUCKET_NAME = "wafer-fault"
+AWS_S3_BUCKET_NAME = "waferfault"
 MONGO_DATABASE_NAME = "pwskills"
 MONGO_COLLECTION_NAME = "waferfault"
 
 
 TARGET_COLUMN = "quality"
-MONGO_DB_URL="mongodb+srv://imran:TdPLW9Ad0OzpSSD2@cluster0.fv0lm61.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+MONGO_DB_URL="mongodb+srv://Vignesh:12345@cluster0.3eufg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 
 MODEL_FILE_NAME = "model"
---
+++ upload_data.py
@@ -0,0 +1,23 @@
+from pymongo.mongo_client import MongoClient
+import pandas as pd
+import json
+
+#url
+
+uri = "mongodb+srv://Vignesh:12345@cluster0.3eufg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+#Create new client and connect to server
+client = MongoClient(uri)
+
+
+#Create a database name and collection name
+DATABASE_NAME = "pwskills"
+COLLECTION_NAME = "waferfault"
+
+df = pd.read_csv("C:\Users\Vignesh\Downloads\Sensor_Project\notebooks\wafer_23012020_041211.csv")
+
+df = df.drop("Unnamed: 0" , axis = 1)
+
+json_record = list(json.loads(df.T.to_json()).values())
+
+client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_record)
